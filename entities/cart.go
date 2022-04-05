82ec6e5c37e8d5e7509df665043d7daa2351bc14
Usamah Abdurrahman <abdurrahman.usamah07@gmail.com>
2022-04-05 22:02:06 +0700

    entites sementara

--- entities/cart.go
+++ entities/cart.go
@@ -8,5 +8,6 @@ type Cart struct {
 	ProductID  uint    `gorm:"not null" json:"product_id" form:"product_id"`
 	Quantity   uint    `gorm:"not null" json:"quantity" form:"quantity"`
 	TotalPrice uint    `gorm:"not null" json:"total_price" form:"total_price"`
-	Product    Product `gorm:"foreignKey:ProductID;references:ID"`
+	Product    Product `gorm:"foreignKey:ProductID;references:ID" json:"product" form:"product"`
+	Order      Order   `gorm:"foreignKey:CartID;references:ID" json:"order" form:"order"`
 }
