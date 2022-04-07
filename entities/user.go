894782befdb14e84399f2c898c165c3e828572f9
haudhi <haudhiizza@gmail.com>
2022-04-07 22:04:27 +0700

    fixing user entities

--- entities/user.go
+++ entities/user.go
@@ -8,8 +8,8 @@ type User struct {
 	Email       string    `gorm:"unique;not null" json:"email" form:"email"`
 	Password    string    `gorm:"not null" json:"password" form:"password"`
 	PhoneNumber string    `gorm:"not null" json:"phone_number" form:"phone_number"`
+	Address     string    `gorm:"not null" json:"address" form:"address"`
 	Product     []Product `gorm:"foreignKey:UserID;references:ID"`
 	Cart        []Cart    `gorm:"foreignKey:UserID;references:ID"`
-	Address     []Address `gorm:"foreignKey:UserID;references:ID"`
 	Order       []Order   `gorm:"foreignKey:UserID;references:ID"`
 }
